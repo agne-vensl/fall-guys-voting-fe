88b7e8be879da25b232b57fb6081ceeed02f6f2a
Agnė Venslavičiūtė <agne.vensl@gmail.com>
2021-07-14 17:46:03 +0300

    register and login routes, logo

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,22 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import Logo from "./components/Logo/Logo";
 import Home from "./pages/Home/Home";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
 
 const Routes = () => {
   return (
     <Router>
+      <Logo
+        src="https://fallguys.com/_next/image?url=%2Fimages%2Fseason4%2Flogo-desktop.png&w=1024&q=75"
+        alt="fall-guys-logo"
+      />
+
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
         <Route path="*" component={Home} />
       </Switch>
     </Router>
