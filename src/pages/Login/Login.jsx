89775e68d858b9b38de61e452d4ea5eafa0fe021
Agnė Venslavičiūtė <agne.vensl@gmail.com>
2021-07-14 17:45:23 +0300

    login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+import axios from "axios";
+
+import Form from "../../components/Form/Form";
+import Button from "../../components/button/Button";
+import StyledLink from "../../components/StyledLink/StyledLink";
+import Notification from "../../components/Notification/Notification";
+
+const Login = () => {
+  const [formData] = useState({ email: "", password: "" });
+  const [notification, setNotification] = useState({});
+
+  const history = useHistory();
+
+  const login = e => {
+    e.preventDefault();
+
+    axios
+      .post(`${process.env.REACT_APP_BASE_URL}/auth/login`, {
+        email: formData.email.trim().toLowerCase(),
+        password: formData.password,
+      })
+      .then(data => {
+        localStorage.setItem("token", data.data.token);
+        history.push("/");
+      })
+      .catch(err => {
+        if (err.response) {
+          return setNotification({
+            type: "error",
+            message: err.response.data.error,
+          });
+        }
+        return setNotification({
+          type: "error",
+          message: "Cannot connect to server. Please try again later",
+        });
+      })
+      .finally(() => {
+        e.target.elements.password.value = "";
+      });
+  };
+
+  return (
+    <>
+      <Form
+        handleSubmit={login}
+        name="login"
+        imageProps={{
+          src:
+            "https://fallguys.com/_next/image?url=%2Fimages%2Fseason4%2Fpopstar-dlc%2Fpunk-idol.png&w=1920&q=75",
+          alt: "fall-guys-future",
+        }}
+      >
+        <div className="links-container">
+          <div className="align-center">
+            <StyledLink to="/login" text="Login" />
+            <StyledLink to="/register" text="Register" />
+          </div>
+        </div>
+
+        <Notification
+          notification={notification}
+          handleDelete={setNotification}
+        />
+
+        <div className="input-field">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            placeholder="email@email.com"
+            required
+            onChange={e => (formData.email = e.target.value)}
+          />
+        </div>
+
+        <div className="input-field">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            placeholder="password"
+            required
+            onChange={e => (formData.password = e.target.value)}
+          />
+        </div>
+
+        <Button type="submit" text="Login" />
+      </Form>
+    </>
+  );
+};
+
+export default Login;
