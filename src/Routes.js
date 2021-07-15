e72d065d2e0fb7338f16d6228d577a9c97906231
Agnė Venslavičiūtė <agne.vensl@gmail.com>
2021-07-15 08:29:20 +0300

    removed logo, dashboard route

--- src/Routes.js
+++ src/Routes.js
@@ -1,23 +1,19 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 
-import Logo from "./components/Logo/Logo";
-import Home from "./pages/Home/Home";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import AnonRoute from "./components/AnonRoute/AnonRoute";
 
 const Routes = () => {
   return (
     <Router>
-      <Logo
-        src="https://fallguys.com/_next/image?url=%2Fimages%2Fseason4%2Flogo-desktop.png&w=1024&q=75"
-        alt="fall-guys-logo"
-      />
-
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/register" component={Register} />
-        <Route path="/login" component={Login} />
-        <Route path="*" component={Home} />
+        <PrivateRoute exact path="/" component={<Dashboard />} />
+        <AnonRoute path="/register" component={<Register />} />
+        <AnonRoute path="/login" component={<Login />} />
+        <PrivateRoute path="*" component={<Dashboard />} />
       </Switch>
     </Router>
   );
