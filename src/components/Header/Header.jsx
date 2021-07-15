f043e8b4c78c61a8794bb87f822d32b5dd679349
Agnė Venslavičiūtė <agne.vensl@gmail.com>
2021-07-15 08:58:41 +0300

    Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import * as S from "./Header.style";
+
+import StyledLink from "../StyledLink/StyledLink";
+
+const Header = () => (
+  <S.Header>
+    <div className="container">
+      <StyledLink
+        to="/login"
+        text="Logout"
+        onClick={() => {
+          localStorage.removeItem("token");
+        }}
+      />
+    </div>
+  </S.Header>
+);
+
+export default Header;
