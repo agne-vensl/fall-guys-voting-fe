06ec786d633922f3bdffa2fe6a8df5d7454e27ed
Agnė Venslavičiūtė <agne.vensl@gmail.com>
2021-07-15 08:59:04 +0300

    hero component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import * as S from "./Hero.style";
+
+import Logo from "../Logo/Logo";
+
+const Hero = ({ title }) => {
+  return (
+    <S.Hero>
+      <div className="container">
+        <Logo
+          src="https://fallguys.com/_next/image?url=%2Fimages%2Fseason4%2Flogo-desktop.png&w=1024&q=75"
+          alt="fall-guys-logo"
+          height="10rem"
+        />
+      </div>
+
+      <S.Title>{title}</S.Title>
+    </S.Hero>
+  );
+};
+
+Hero.propTypes = {
+  title: PropTypes.string,
+};
+
+Hero.defaultProps = {
+  title: undefined,
+};
+
+export default Hero;
---
+++ src/components/Hero/Hero.style.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Hero = styled.div`
+  margin-bottom: 3rem;
+
+  &.container {
+    padding: 0 1rem;
+  }
+
+  @media only screen and (min-width: 768px) {
+    margin-bottom: 6rem;
+  }
+`;
+
+export const Title = styled.h1`
+  color: #4fc1ff;
+  font-size: 12vw;
+  margin: 0 auto;
+  margin-top: -2.5rem;
+  position: relative;
+  text-align: center;
+  text-shadow: 0 0 0.7em #4fc1ff;
+  text-transform: uppercase;
+  transform: rotate(-10deg);
+  width: fit-content;
+  z-index: 2;
+
+  @media only screen and (min-width: 768px) {
+    font-size: 6rem;
+    margin-top: -4rem;
+  }
+`;
