44e927b9971887f94af0b883319bfaa17cbfe985
Agnė Venslavičiūtė <agne.vensl@gmail.com>
2021-07-15 08:43:45 +0300

    styles, handleClick

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,22 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 import * as S from "./Button.style";
 
-const Button = ({ color, type, text, ...props }) => (
-  <S.Button color={color} type={type} {...props}>
+const Button = ({ color, type, text, handleClick, ...props }) => (
+  <S.Button color={color} type={type} onClick={handleClick} {...props}>
     {text}
   </S.Button>
 );
 
 Button.propTypes = {
-  color: PropTypes.oneOf(["primary", "secondary"]),
+  color: PropTypes.oneOf(["primary", "secondary", "success"]),
   type: PropTypes.oneOf(["button", "submit", "reset"]),
-  text: PropTypes.string,
+  text: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  handleClick: PropTypes.func,
 };
 
 Button.defaultProps = {
   color: "primary",
   type: "button",
   text: undefined,
+  handleClick: undefined,
 };
 
 export default Button;
--- src/components/button/Button.style.js
+++ src/components/button/Button.style.js
@@ -3,38 +3,27 @@ import styled from "styled-components";
 const colors = {
   primary: "#fc86cd",
   secondary: "#eee",
+  success: "#4fc1ff",
+  danger: "#a72020",
 };
 
 export const Button = styled.button`
-  /* background-color: ${props =>
-    props.color === "primary" ? "blue" : "grey"};
-  border: 0;
-  color: #fff;
-  cursor: pointer;
-  padding: 0.8rem 2rem; */
-  /* position: relative; */
-  /* font-family: "Montserrat", Arial, sans-serif; */
-  /* font-size: calc(20px + 5vw); */
   background-color: transparent;
   border: 2px solid ${props => colors[props.color]};
   border-radius: 5px;
-  cursor: pointer;
-  /* font-weight: 700; */
-  color: ${props => colors[props.color]};
-  letter-spacing: 0.02em;
-  text-transform: uppercase;
-  /* #1da9cc */
-  text-shadow: 0 0 0.7em ${props => colors[props.color]};
   box-shadow: 0 0 0.7em ${props => colors[props.color]};
-  /* user-select: none; */
-  /* white-space: nowrap; */
+  cursor: pointer;
+  color: ${props => colors[props.color]};
   filter: blur(0.007em);
+  letter-spacing: 0.02em;
   padding: 0.8rem 2rem;
+  text-transform: uppercase;
+  text-shadow: 0 0 0.7em ${props => colors[props.color]};
   transition: all 0.2s ease-in-out;
 
   &:hover {
-    text-shadow: 0 0 0.5em ${props => colors[props.color]};
     box-shadow: 0 0 0.5em ${props => colors[props.color]};
+    text-shadow: 0 0 0.5em ${props => colors[props.color]};
   }
 
   &:active {
