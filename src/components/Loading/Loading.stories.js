0bb189e555f44e1168a47bd20ea6fe63fa2cd7eb
Agnė Venslavičiūtė <agne.vensl@gmail.com>
2021-07-16 15:51:49 +0300

    Loading component

---
+++ src/components/Loading/Loading.stories.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Loading from "./Loading";
+
+export default {
+  title: "Components/Loading",
+  component: Loading,
+  argTypes: {
+    color: {
+      options: ["primary"],
+    },
+  },
+};
+
+const Template = args => (
+  <div style={{ background: "#222", padding: "2rem" }}>
+    <Loading {...args} />
+  </div>
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  color: "primary",
+};
