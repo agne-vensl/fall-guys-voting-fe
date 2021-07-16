2116c339b4c7a21d0074236fad9cae86d0fcc6c0
Agnė Venslavičiūtė <agne.vensl@gmail.com>
2021-07-16 14:27:01 +0300

    object fit, object cover for img

--- src/components/Card/Card.style.js
+++ src/components/Card/Card.style.js
@@ -39,6 +39,8 @@ export const ImageContainer = styled.div`
 export const Image = styled.img`
   height: 100%;
   width: 100%;
+  object-fit: cover;
+  object-position: center;
   padding: 0.75rem;
 `;
 
