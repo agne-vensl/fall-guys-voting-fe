2043a87007a06a88767e7c17d7ef0302d521b3ba
Agnė Venslavičiūtė <agne.vensl@gmail.com>
2021-07-17 14:57:51 +0300

    added component imports

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+export { default as AnonRoute } from "./AnonRoute/AnonRoute";
+export { default as Button } from "./Button/Button";
+export { default as Card } from "./Card/Card";
+export { default as CardsContainer } from "./CardsContainer/CardsContainer";
+export { default as Form } from "./Form/Form";
+export { default as Header } from "./Header/Header";
+export { default as Hero } from "./Hero/Hero";
+export { default as Loading } from "./Loading/Loading";
+export { default as Logo } from "./Logo/Logo";
+export { default as Notification } from "./Notification/Notification";
+export { default as PrivateRoute } from "./PrivateRoute/PrivateRoute";
+export { default as StyledLink } from "./StyledLink/StyledLink";
